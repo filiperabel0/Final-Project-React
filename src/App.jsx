4a4906b75aebe6a3f1fa69e8fdd5118b46663d62
filiperabel0 <filiperabelo960@yahoo.com.br>
2022-11-23 23:13:48 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,114 @@
 import { useState } from "react";
-
 import "./App.css";
+import SelectForm from "./SelectForm";
+import styled from "styled-components";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div className="main-container" style={{ display: "flex", flexDirection: "row", justifyContent: "center", width: "100vh", height: "100vh" }}>
-      <div>
-        <div className="container">
-          <div className="container-login">
-            <div className="wrap-login">
-              <form className="login-form">
-                <span className="login-form-title"> Cadastro Professores</span>
-                <span className="login-form-title">
-                </span>
-                <div className="wrap-input">
-                  <input
-                    className={email !== "" ? "has-val input" : "input"}
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <span className="focus-input" data-placeholder="Professor"></span>
-                </div>
-                <div className="container-login-form-btn">
-                  <button className="login-form-btn">CADASTRAR</button>
-                </div>
-              </form>
+    <div className="div-Geral">
+      <div
+        className="main-container"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          width: "100vh",
+          height: "100vh",
+        }}
+      >
+        <div>
+          <div className="container">
+            <div className="container-login">
+              <div className="wrap-login">
+                <form className="login-form">
+                  <span className="login-form-title">
+                    {" "}
+                    Cadastro Professores
+                  </span>
+                  <span className="login-form-title"></span>
+                  <div className="wrap-input">
+                    <input
+                      className={email !== "" ? "has-val input" : "input"}
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <span
+                      className="focus-input"
+                      data-placeholder="Professor"
+                    ></span>
+                  </div>
+                  <div className="container-login-form-btn">
+                    <button className="login-form-btn">CADASTRAR</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="container">
+            <div className="container-login">
+              <div className="wrap-login">
+                <form className="login-form">
+                  <span className="login-form-title"> Cadastro Disciplina</span>
+                  <span className="login-form-title"></span>
+                  <div className="wrap-input">
+                    <input
+                      className={email !== "" ? "has-val input" : "input"}
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <span
+                      className="focus-input"
+                      data-placeholder="Disciplina"
+                    ></span>
+                  </div>
+                  <div className="container-login-form-btn">
+                    <button className="login-form-btn">CADASTRAR</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="container">
+            <div className="container-login">
+              <div className="wrap-login">
+                <form className="login-form">
+                  <span className="login-form-title"> Cadastro Turma</span>
+                  <span className="login-form-title"></span>
+                  <div className="wrap-input">
+                    <input
+                      className={email !== "" ? "has-val input" : "input"}
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <span
+                      className="focus-input"
+                      data-placeholder="Turma"
+                    ></span>
+                  </div>
+                  <div className="container-login-form-btn">
+                    <button className="login-form-btn">CADASTRAR</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div>
-        <div className="container">
-          <div className="container-login">
-            <div className="wrap-login">
-              <form className="login-form">
-                <span className="login-form-title"> Cadastro Disciplina</span>
-                <span className="login-form-title">
-                </span>
-                <div className="wrap-input">
-                  <input
-                    className={email !== "" ? "has-val input" : "input"}
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <span className="focus-input" data-placeholder="Disciplina"></span>
-                </div>
-                <div className="container-login-form-btn">
-                  <button className="login-form-btn">CADASTRAR</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div className="container">
-          <div className="container-login">
-            <div className="wrap-login">
-              <form className="login-form">
-                <span className="login-form-title"> Cadastro Turma</span>
-                <span className="login-form-title">
-                </span>
-                <div className="wrap-input">
-                  <input
-                    className={email !== "" ? "has-val input" : "input"}
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <span className="focus-input" data-placeholder="Turma"></span>
-                </div>
-                <div className="container-login-form-btn">
-                  <button className="login-form-btn">CADASTRAR</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <SelectForm />
     </div>
-
   );
 }
 
-export default App;
+export default styled(App)``;
---
+++ src/SelectForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+function SelectForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+     <div>
+        <form action="">
+          <input list="professores" name="professores"/>
+            <datalist id="professores">
+              <option value="Sandra">Sandra</option>
+              <option value="Cleber">Cleber</option>
+              <option value="Conceição">Conceição</option>
+              <option value="Rodolfo">Rodolfo</option>
+              <option value="Eder">Eder</option>
+              <option value="Mateus">Mateus</option>
+            </datalist>
+            
+        </form>
+
+        <form action="">
+          <input list="disciplina" name="disciplina"/>
+            <datalist id="disciplina">
+              <option value="Framework">Framework</option>
+              <option value="Atualidades">Atualidades</option>
+              <option value="Matemática">Matemática</option>
+              <option value="Filosofia">Filosofia</option>
+              <option value="IOS">IOS</option>
+              <option value="Android">Android</option>
+            </datalist>
+          
+        </form>
+
+        <form action="">
+          <input list="turma" name="turma"/>
+            <datalist id="turma">
+              <option value="3A">3A</option>
+              <option value="3B">3B</option>
+              <option value="3C">3C</option>
+              <option value="3A2">3A2</option>
+              <option value="3B2">3B2</option>
+              <option value="3C2">3C2</option>
+            </datalist>
+            
+        </form>
+
+        <form>
+            <input type="text" id="days" placeholder="Numero de Dias" />
+        </form>
+
+        <input type="submit" />
+      </div>
+
+  )
+}
+export default SelectForm;
